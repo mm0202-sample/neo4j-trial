b63f786224eaa9ce56cd8893e2eb13a4e617fa0c
mm0202 <for_etc@hotmail.com>
2020-06-21 03:51:24 +0900

    SB-420 FOLLOWリレーション検索追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Node } from "./packages/@types/App";
+import { Elements, Node } from "./packages/@types/App";
 import { getAllPersons } from "./packages/Search/getAllPersons";
 import Search from "./packages/Search/Search";
 
 function App() {
+    const [elements, setElements] = useState<Elements>({ nodes: [], edges: [] })
     const [persons, setPersons] = useState<Node<any>[] | []>([])
 
     useEffect(() => {
@@ -13,8 +14,20 @@ function App() {
     return (
         <div>
             <div>
-                <Search persons={persons}/>
+                <Search persons={persons} handleSearchButtonClick={(elements) => setElements(elements)}/>
             </div>
+            {elements.nodes.length > 0 && <div>
+                <hr/>
+                <h3>nodes</h3>
+                <ul>
+                    {elements.nodes.map((node) => <li>{JSON.stringify(node.data)}</li>)}
+                </ul>
+                <hr/>
+                <h3>edges</h3>
+                <ul>
+                    {elements.edges.map((edge) => <li>{JSON.stringify(edge.data)}</li>)}
+                </ul>
+            </div>}
         </div>
     )
 }
--- src/packages/Search/Search.tsx
+++ src/packages/Search/Search.tsx
@@ -1,13 +1,15 @@
 import Autocomplete from "@material-ui/lab/Autocomplete";
-import { Node } from "../@types/App";
-import { TextField } from "@material-ui/core";
+import { Elements, Node } from "../@types/App";
+import { Button, TextField } from "@material-ui/core";
 import React, { ChangeEvent, useState } from "react";
+import { getElements } from "./getElements";
 
 type Props = {
     persons: Node<any>[] | []
+    handleSearchButtonClick: (elements: Elements) => void
 }
 
-export default function Search({ persons }: Props) {
+export default function Search({ persons, handleSearchButtonClick }: Props) {
     const [target, setTarget] = useState<Node<any>>(null)
 
     const handleTargetChange = (event: ChangeEvent<{}>, newTarget: Node<any>) => setTarget(newTarget)
@@ -24,5 +26,8 @@ export default function Search({ persons }: Props) {
                 <TextField {...params} label="target" variant="outlined"/>
             )}
         />
+        <Button onClick={async () => handleSearchButtonClick(await getElements(target))} variant={'outlined'}>
+            search
+        </Button>
     </div>
 }
